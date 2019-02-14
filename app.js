f4e198cd3690a502f12efb8ca90e5df6f77fa9d1
zivshek <ziv.hao.shek@gmail.com>
2019-02-13 21:56:06 -0500

    added the p5.dom.js lib, added two color pickers, added some initial boids

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 let boids = function (p) {
-
+    let mycanvas;
     let canvasW = 800;
     let canvasH = 600;
 
@@ -16,27 +16,57 @@ let boids = function (p) {
     let boids;
     let totalBoids = 100;
 
-    let lol;
+    let boidsColorPicker, bkgColorPicker;
+    let spacing = 5;
 
-    let colorPicker;
+    let boidsColor = p.color('black');
+    let bkgColor = p.color('white');
 
     p.setup = function() {
         
-        p.createCanvas(canvasW, canvasH);
-        colorPicker = p.createColorPicker('#ffffff');
-        colorPicker.position(marginXR, marginY);
-        //boids = Array(100)
+        mycanvas = p.createCanvas(canvasW, canvasH);
+        mycanvas.mousePressed(p.mouseHandler);
+        boidsColorPicker = p.createColorPicker(boidsColor);
+        boidsColorPicker.position(marginXR, marginY + boidsColorPicker.height / 2);
+        boidsColorPicker.input(p.setBoidsColor);
+
+        bkgColorPicker = p.createColorPicker(bkgColor);
+        bkgColorPicker.position(marginXR, marginY + bkgColorPicker.height / 2 + spacing + boidsColorPicker.height);
+        bkgColorPicker.input(p.setBkgColor);
+        p.textSize(15);
+        boids = Array(100).fill().map(x => new Boid(p, gameCanvasX, gameCanvasY));
     };
 
     p.draw = function() {
         p.background(200);
         p.gameCanvas();
-        
+        for (let b of boids) {
+            b.draw(boidsColor);
+        }
+    };
+
+    p.mouseHandler = function() {
+
+    };
+
+    p.setBoidsColor = function() {
+        boidsColor = boidsColorPicker.color();
+    };
+
+    p.setBkgColor = function() {
+        bkgColor = bkgColorPicker.color();
     };
 
     p.gameCanvas = function() {
+        p.fill(bkgColor);
+        p.stroke(1);
         p.rectMode(p.CENTER);
         p.rect(gameCanvasX, gameCanvasY, gameCanvasW, gameCanvasH);
+        
+        p.noStroke();
+        p.fill(0, 102, 153);
+        p.text('Boids Color', marginXR + boidsColorPicker.width + spacing, marginY + boidsColorPicker.height);
+        p.text('Background Color', marginXR + boidsColorPicker.width + spacing, marginY + boidsColorPicker.height + spacing + bkgColorPicker.height);
     }
 };
 
--- boid.js
+++ boid.js
@@ -1,15 +1,19 @@
 class Boid {
 
-    constructor(p5){
+    constructor(p5, x, y){
         this.p5 = p5;
+        this.pos = this.p5.createVector(x, y);
+        this.size = 20;
+        this.moveSpeed = 10;
+        this.velocity = this.p5.createVector(Math.random() * 2 - 1, Math.random() * 2 - 1);
 
-        this.acc = 0;
     }
 
     draw(color){
-        this.p5.rectMode(this.p5.CORNER);
-        this.p5.stroke(0);
+        this.pos.add(this.velocity);
+
+        this.p5.noStroke();
         this.p5.fill(color);
-        this.p5.rect(this.x, this.y, this.size, this.size);
+        this.p5.triangle(this.pos.x, this.pos.y - 5, this.pos.x - 2, this.pos.y + 2, this.pos.x + 2, this.pos.y + 2);
     }
 }
